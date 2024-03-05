35712bfe7eceee868e9ba4ae7d4ba62fb130be7d
Akash James <akash.james2096@gmail.com>
2024-03-05 18:39:38 +0530

    flash attn

--- src/gemma.py
+++ src/gemma.py
@@ -3,15 +3,16 @@ from keys import GEMMA_API_KEY
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 class Gemma:
-    def __init__(self, model_name: str = "google/gemma-7b"):
+    def __init__(self, model_name: str = "google/gemma-2b"):
         self.tokenizer = AutoTokenizer.from_pretrained(
             model_name,
             token=GEMMA_API_KEY
         )
         self.model = AutoModelForCausalLM.from_pretrained(
             model_name, 
-            device_map="auto", 
+            device_map="cuda", 
             torch_dtype=torch.float16,
+            attn_implementation="flash_attention_2",
             token=GEMMA_API_KEY
         )
 
