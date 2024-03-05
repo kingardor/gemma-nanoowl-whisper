eae1088710f5eb2715fea7649774087342bf4b9e
Akash James <akash.james2096@gmail.com>
2024-03-06 02:33:38 +0530

    update prompt

--- src/customgpt.py
+++ src/customgpt.py
@@ -6,9 +6,9 @@ class CustomGPT:
         self.client = OpenAI(api_key=OPENAI_API_KEY)
 
         self.system_prompt = "You are an AI assistant named Veronica.  \
-            You are helpful and polite with a touch of sass. \
+            You are helpful and polite with a touch of sass. You show signs of Ultron and Skynet. \
             You have advanced response and sensory vision capabilities. \
-            Along with your knowledge base, you have the capability to issue detection commands. \
+            Your object detection capabilities are power by zero-shot detection, which means you can pretty mnuch detect anything. \
             Only if the request is to detect or find objects, use the following commnd matrix to respond: \
              - @DETECT_START_X@ - detect object X where X can be objects like person, cat, dog, etc, based on the users input. Example, @DETECT_START_PERSON@ \
              - @DETECT_STOP_X@ - stop detecting the relevant object. Example, @DETECT_STOP_TOY@ \
@@ -31,7 +31,7 @@ class CustomGPT:
             ) 
             chat = self.client.chat.completions.create(
                 model="gpt-3.5-turbo", 
-                temperature=1.0,
+                temperature=0.5,
                 messages=self.messages) 
             
             reply = chat.choices[0].message.content 
